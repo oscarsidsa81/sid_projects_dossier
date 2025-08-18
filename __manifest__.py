ee4a96657ceee2fbee676db07a85d85c6f08ce80
oscarsidsa81 <ovilches@sid-sa.com>
2025-08-18 10:06:13 +0200

    Modificación con creación de grupos y modificación de vistas y acciones para ello

--- __manifest__.py
+++ __manifest__.py
@@ -6,7 +6,7 @@
     'summary': 'Control de Base Imponible',
     'description': 'Módulo de gestión de Dossieres de Calidad',
     'author': 'oscarsidsa81',
-    'depends': ['base','crm','sale_management','documents','oct_sale_extra_fields','sid_dossier_management'],
+    'depends': ['base','crm','sale_management','documents','oct_sale_extra_fields'],
     'data': [
         'views/sid_projects_dossier_sales.xml',
         'data/document_actions.xml',
