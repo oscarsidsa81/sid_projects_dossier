4533e092fe0eb5ad9baff1d4dfdb50b1e72a044d
oscarsidsa81 <ovilches@sid-sa.com>
2026-02-10 11:41:03 +0100

    Production

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'sid_projects_dossier',
-    'version': '1.0.1',
+    'version': '1.0.2',
     'category': 'Sales',
     'license': 'AGPL-3',
     'summary': 'Control de Base Imponible',
