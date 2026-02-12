bf2dae38c3ab2818516942df6cc941875102f078
oscarsidsa81 <ovilches@sid-sa.com>
2026-02-12 13:23:53 +0100

    Production

--- models/sid_sale_quotations_dossier.py
+++ models/sid_sale_quotations_dossier.py
@@ -71,9 +71,15 @@ class SaleQuotationsDossier(models.Model):
         return {
             'type': 'ir.actions.act_window',
             'name': _('Dossier'),
-            'res_model': 'documents.folder',
-            'view_mode': 'form',
-            'res_id': folder.id,
+            'res_model': 'documents.document',
+            'view_mode': 'tree,kanban',
+            'domain': [('folder_id', 'child_of', folder.id)],
+            'context': {
+                'default_folder_id': folder.id,
+                'searchpanel_default_folder_id': folder.id,
+                'searchpanel_default_folder_id_domain': [('folder_id', 'child_of', folder.id)],
+                'group_by': ['folder_id'],
+            },
             'target': 'current',
         }
 
