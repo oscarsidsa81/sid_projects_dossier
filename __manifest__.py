a21f5123a688ee0c6e36fa72a6b00dc5406d31cd
oscarsidsa81 <ovilches@sid-sa.com>
2026-04-06 11:24:55 +0200

    review manifest

--- __manifest__.py
+++ __manifest__.py
@@ -6,7 +6,7 @@
     'summary': 'Control de Base Imponible',
     'description': 'Módulo de gestión de Dossieres de Calidad',
     'author': 'oscarsidsa81',
-    'depends': ['base','crm','sale_management','documents','oct_sale_extra_fields'],
+    'depends': ['base','crm','sale_management','documents','oct_sale_extra_fields', 'sid_bankbonds_mod'],
     'data': [
         'security/security.xml',
         'security/ir.model.access.csv',
